3fb943359a08233f866f7a7723ea17ff0b6dc41f
Takumi Kimura <shachibou@macbook-pro-7.local>
2018-12-01 00:01:19 +0900

    Initial Commit

---
+++ hangman-game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hangman-game
+//
+//  Created by Takumi Kimura on 2018/12/01.
+//  Copyright © 2018年 com.takumi0kimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
